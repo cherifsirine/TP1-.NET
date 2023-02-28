de5995d9c4a2f348fe9503adbab55ea964aa1ceb
cherifsirine <126579906+cherifsirine@users.noreply.github.com>
2023-02-28 14:17:53 +0100

    Add files via upload

---
+++ Tp1/Tp1/Epreuve.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp1
+{
+    internal class Epreuve
+    {
+        private string id, nom;
+        private List <Athlete> listeA;
+        private List <Resultat> listeR;
+        private bool ferme;
+        public Epreuve()
+        {
+            id = "";
+            nom = "";
+            listeA = new List <Athlete>();
+            listeR = new List <Resultat>();
+            ferme = false;
+        }
+        public bool AjouterAthlete( Athlete a )
+        {
+            if (listeA.Contains(a)) {
+                Console.WriteLine("L'athlète existe déjà");
+                    return false;
+            
+            }else { try
+                {
+                    listeA.Add(a);
+                    return true;
+                }catch(NullReferenceException x)
+                {
+                    Console.WriteLine("Athlète null");
+                    return false;
+                }
+                }
+        }
+    }
+    
+}
---
+++ Tp1/Tp1/Programme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp1
+{
+    internal class Programme
+    {
+        public static void Main(string[] args)
+        {
+            Athlete A = new Athlete();
+            A.saisir();
+        }
+    }
+}
